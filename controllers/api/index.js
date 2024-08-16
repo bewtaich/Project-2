ddf26772fb16323c549c0ea029bb0edad79e420a
Jarvis <jarvwashington@gmail.com>
2024-08-16 14:29:09 -0500

    updates to the comment model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
+const commentsRoutes = require('./commentsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/upload', blogRoutes);
+router.use('/sub', commentsRoutes);
 module.exports = router;
