6553f350f1289f1b94be7b357036beeebfc3bd3e
Chris Moore <chrismoorelmt@gmail.com>
2024-08-16 10:34:15 -0500

    Removed comment tics in navbuttons.js

--- public/js/navbuttons.js
+++ public/js/navbuttons.js
@@ -1,11 +1,11 @@
-// const home = async (event) => {
-//     event.preventDefault();
-//     document.location.replace('/')
-//   }
+ const home = async (event) => {
+     event.preventDefault();
+     document.location.replace('/')
+   }
 
 const profile = async (event) => {
     event.preventDefault();
     document.location.replace('/profile')
 }
-  // document.querySelector('#home').addEventListener('click', home);
+   document.querySelector('#home').addEventListener('click', home);
   document.querySelector('#profile').addEventListener('click', profile);
