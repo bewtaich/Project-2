21c721d182ef0c55098be1df1468a64bf9fac4e5
Jarvis <jarvwashington@gmail.com>
2024-08-19 09:02:42 -0500

    update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,9 +15,9 @@ router.get("/", async (req, res) => {
     
     
     const posts = blogPosts.map((post) => post.get({ plain: true })); //making database data useful for front end.
-    const allComments= posts.map((post) => post.Comments);
-    console.log(posts);
-    console.log(posts[0].Comments);
+    // const allComments= posts.map((post) => post.Comments);
+    // console.log(posts);
+    // console.log(posts[0].Comments);
 
 
 
